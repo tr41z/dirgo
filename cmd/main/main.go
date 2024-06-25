285efd6a41dd0325a3ac256776f764dae441d8aa
traizooo <michal.owsiejew23@gmail.com>
2024-06-25 21:29:09 +0200

    Commit

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -11,7 +11,7 @@ func main() {
 	var urlFlag = flag.String("u", "", "URL in format (e.g., http://192.168.68.74:8080)")
 	var statusFlag = flag.Int("s", 200, "HTTP status code (e.g., 200 for OK)")
 	var wordlistFlag = flag.String("w", "", "Path to the wordlist file")
-	var threadsFlag = flag.Int("t", 10, "Number of concurrent threads")
+	var threadsFlag = flag.Int("t", 5, "Number of concurrent threads")
 
 	flag.Parse()
 
